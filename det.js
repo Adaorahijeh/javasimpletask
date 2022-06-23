c4713c5761fdf201c7659a38af0eb320d0790d61
adaorah ijeh <adaorahijeh8@gmail.com>
2022-06-23 22:43:24 +0100

    first commit

---
+++ det.js
@@ -0,0 +1,6 @@
+let person = {
+    name : "Adaorah",
+    height : 5.5, 
+    country : "Nigeria",
+}
+alert('My name is '+ person.name +'.\n I am '+ person.height + 'ft in height, and I am from ' + person.country +'.');
